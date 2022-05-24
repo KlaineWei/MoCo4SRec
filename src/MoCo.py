fix queue size overflow

src/MoCo.py
@@ -60,13 +60,21 @@ class MoCo(nn.Module):
         # keys = concat_all_gather(keys)
 
         batch_size = keys.shape[0]
+        # print("batch size:{}", batch_size)
+        # print("queue shape:{}", self.queue.shape)
 
         ptr = int(self.queue_ptr)
+        # print("ptr:{}", ptr)
         # assert self.K % batch_size == 0  # for simplicity
 
         # replace the keys at ptr (dequeue and enqueue)
-        self.queue[:, ptr:ptr + batch_size] = keys.T
+        if ptr + batch_size >= self.K:
+            self.queue[:, ptr:self.K] = keys[:self.K-ptr].T
+            self.queue[:, :ptr + batch_size - self.K] = keys[self.K-ptr:].T
+        else:
+            self.queue[:, ptr:ptr + batch_size] = keys.T
         ptr = (ptr + batch_size) % self.K  # move pointer
+        # print("ptr:{}", ptr)
 
         self.queue_ptr[0] = ptr
 

src/models.py
@@ -71,7 +71,7 @@ class SASRecModel(nn.Module):
         return sequence_output
 
     # moco
-    def moco_encoder(self, input_ids):
+    def moco_trans_encoder(self, input_ids):
 
         attention_mask = (input_ids > 0).long()
         extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)  # torch.int64

src/trainers.py
@@ -202,7 +202,7 @@ class CoSeRecTrainer(Trainer):
         """
         moco_batch = torch.cat(inputs, dim=0)
         moco_batch = moco_batch.to(self.device)
-        moco_logits, moco_labels = self.model.moco_encoder(moco_batch, self.device)
+        moco_logits, moco_labels = self.model.moco_trans_encoder(moco_batch)
         criterion = nn.CrossEntropyLoss().cuda(0)
         moco_loss = criterion(moco_logits,
                               moco_labels)