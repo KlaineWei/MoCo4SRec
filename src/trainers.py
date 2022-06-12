6be7d65d2b4a428ecb462314b85781cfa54bc6ba
weizihan <weizihan1021@gmail.com>
2022-06-12 14:54:46 +0800

    add cl loss

--- src/trainers.py
+++ src/trainers.py
@@ -219,12 +219,14 @@ class CoSeRecTrainer(Trainer):
             rec_avg_loss = 0.0
             cl_individual_avg_losses = [0.0 for i in range(self.total_augmentaion_pairs)]
             cl_sum_avg_loss = 0.0
+            moco_individual_avg_losses = [0.0 for i in range(self.total_augmentaion_pairs)]
+            moco_sum_avg_loss = 0.0
             joint_avg_loss = 0.0
 
             print(f"rec dataset length: {len(dataloader)}")
             rec_cf_data_iter = tqdm(enumerate(dataloader), total=len(dataloader))
 
-            for i, (rec_batch, cl_batches) in rec_cf_data_iter:
+            for i, (rec_batch, cl_batches, moco_batches) in rec_cf_data_iter:
                 """
                 rec_batch shape: key_name x batch_size x feature_dim
                 cl_batches shape: 
@@ -241,13 +243,19 @@ class CoSeRecTrainer(Trainer):
                 # ---------- contrastive learning task -------------#
                 cl_losses = []
                 for cl_batch in cl_batches:
-                    cl_loss = self._moco_pair_contrastive_learning(cl_batch)
-                    # cl_loss += self._one_pair_contrastive_learning(cl_batch)
+                    cl_loss = self._one_pair_contrastive_learning(cl_batch)
                     cl_losses.append(cl_loss)
+
+                moco_losses = []
+                for moco_batch in moco_batches:
+                    moco_loss = self._moco_pair_contrastive_learning(moco_batch)
+                    moco_losses.append(moco_loss)
 
                 joint_loss = self.args.rec_weight * rec_loss
                 for cl_loss in cl_losses:
                     joint_loss += self.args.cf_weight * cl_loss
+                for moco_loss in moco_losses:
+                    joint_loss += self.args.moco_weight * moco_loss
                 self.optim.zero_grad()
                 joint_loss.backward()
                 self.optim.step()
@@ -257,6 +265,10 @@ class CoSeRecTrainer(Trainer):
                 for i, cl_loss in enumerate(cl_losses):
                     cl_individual_avg_losses[i] += cl_loss.item()
                     cl_sum_avg_loss += cl_loss.item()
+                for i, moco_loss in enumerate(moco_losses):
+                    moco_individual_avg_losses[i] += moco_loss.item()
+                    moco_sum_avg_loss += moco_loss.item()
+
                 joint_avg_loss += joint_loss.item()
 
             post_fix = {
@@ -265,10 +277,15 @@ class CoSeRecTrainer(Trainer):
                 "joint_avg_loss": '{:.4f}'.format(joint_avg_loss / len(rec_cf_data_iter)),
                 "cl_avg_loss": '{:.4f}'.format(
                     cl_sum_avg_loss / (len(rec_cf_data_iter) * self.total_augmentaion_pairs)),
+                "moco_avg_loss": '{:.4f}'.format(
+                    moco_sum_avg_loss / (len(rec_cf_data_iter) * self.total_augmentaion_pairs)),
             }
             for i, cl_individual_avg_loss in enumerate(cl_individual_avg_losses):
                 post_fix['cl_pair_' + str(i) + '_loss'] = '{:.4f}'.format(
                     cl_individual_avg_loss / len(rec_cf_data_iter))
+            for i, moco_individual_avg_loss in enumerate(moco_individual_avg_losses):
+                post_fix['moco_pair_' + str(i) + '_loss'] = '{:.4f}'.format(
+                    moco_individual_avg_loss / len(rec_cf_data_iter))
 
             if (epoch + 1) % self.args.log_freq == 0:
                 print(str(post_fix))
